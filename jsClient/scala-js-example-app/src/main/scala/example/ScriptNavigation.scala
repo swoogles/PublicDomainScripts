6fb35c48d5f13776e53cf6a31dc18fdf1ec21e72
swoogles <bill.frasure@gmail.com>
2019-09-28 23:15:43 -0600

    Revert "Get rid of mutable next/prev line abominations"
    This reverts commit ecec7cadab0896b832182a004bcd00fedc22a5d5.

--- jsClient/scala-js-example-app/src/main/scala/example/ScriptNavigation.scala
+++ jsClient/scala-js-example-app/src/main/scala/example/ScriptNavigation.scala
@@ -4,32 +4,34 @@ import org.scalajs.dom
 import dom.document.getElementById
 import org.scalajs.jquery.{JQuery, JQueryEventObject}
 
-class CurrentTargets(
-               val previousLineId: String,
-               val nextLineId: String
-             )
-
 object ScriptNavigation {
   var targetCharacterLines: JQuery = _
+  var previousLineId: String = ""
+  var nextLineId: String = ""
 
   val TARGET_SCRIPT_VARIATION =
 //    "full_script_with_lines_highlighted"
   "completely_blank_lines_with_spoken_cues"
 
-  private def iterateToElement(targetId: (CurrentTargets) => String, numSteps: Int, scrollingTarget: ScrollingTarget, currentTargets: CurrentTargets): Unit = {
-    val targetLine = getElementById(targetId(currentTargets))
+  private def iterateToElement(targetId: () => String, numSteps: Int, scrollingTarget: ScrollingTarget): Unit = {
+    val targetLine = getElementById(targetId())
+    nextLineId = targetLine.getAttribute("data-next-line")
+    previousLineId = targetLine.getAttribute("data-previous-line")
 
     if( numSteps > 0)
-      iterateToElement(targetId, numSteps -1, scrollingTarget, new CurrentTargets(targetLine.getAttribute("data-previous-line"), targetLine.getAttribute("data-next-line") ))
+      iterateToElement(targetId, numSteps -1, scrollingTarget)
     else
-      scrollToElementWithBuffer(targetId, scrollingTarget, new CurrentTargets(targetLine.getAttribute("data-previous-line"), targetLine.getAttribute("data-next-line") ))
+      scrollToElementWithBuffer(targetId, scrollingTarget)
   }
 
-  private def scrollToElementWithBuffer(targetId: (CurrentTargets) => String, scrollingTarget: ScrollingTarget, currentTargets: CurrentTargets): Unit = {
-    val fixedTargetId: String = targetId(currentTargets)
+  private def scrollToElementWithBuffer(targetId: () => String, scrollingTarget: ScrollingTarget): Unit = {
+    val fixedTargetId: String = targetId()
 
     val targetLine = getElementById(fixedTargetId)
     val targetLineTyped = ConnectedLine(targetLine)
+
+    nextLineId = targetLineTyped.nextLineId
+    previousLineId = targetLineTyped.previousLineId
 
     if(dom.document.URL.contains(s"$TARGET_SCRIPT_VARIATION/")) { // What an ugly way to work with this for the time being
       targetLineTyped.cueLine.scrollIntoView(true)
@@ -44,7 +46,6 @@ object ScriptNavigation {
     // TODO get query param character here.
     // dom.document.URL
     // val fields=temp_url.split("&").map(js.URIUtils.decodeURIComponent)
-    println("way Less mutable bullshit!!")
     val targetCharacterWithPrefix = dom.window.location.toString.dropWhile(_ != '=')
     val targetCharacterAttempt: Option[String] = if (!targetCharacterWithPrefix.isEmpty) Some(targetCharacterWithPrefix.tail) else None
     targetCharacterAttempt.foreach( targetCharacter => { // Only setup controls if there is a character selected
@@ -63,29 +64,27 @@ object ScriptNavigation {
       }
 
         val firstCharacterLine = targetCharacterLines.get(0)
+        nextLineId = firstCharacterLine.id
+        previousLineId = firstCharacterLine.id
+
+        def getPrevLineId(): String = previousLineId
+
+        def getNextLineId(): String = nextLineId
 
         targetCharacterLines
           .click(ContentHiding.toggleContent _)
 
-        val currentTargets = new CurrentTargets(firstCharacterLine.id, firstCharacterLine.id)
-
-      def getPreviousLineFromTargets(currentTargets: CurrentTargets) =
-        currentTargets.previousLineId
-
-      def getNextLineFromTargets(currentTargets: CurrentTargets) =
-        currentTargets.nextLineId
-
         jquery(".scroll-to-next-line")
-          .click { _: JQueryEventObject => scrollToElementWithBuffer(getNextLineFromTargets, Next, currentTargets) }
+          .click { _: JQueryEventObject => scrollToElementWithBuffer(getNextLineId, Next) }
 
         jquery(".scroll-to-next-line-big")
-          .click { _: JQueryEventObject => iterateToElement(getNextLineFromTargets, 10, Next, currentTargets) }
+          .click { _: JQueryEventObject => iterateToElement(getNextLineId, 10, Next) }
 
         jquery(".scroll-to-previous-line")
-          .click { _: JQueryEventObject => scrollToElementWithBuffer(getPreviousLineFromTargets, Prev, currentTargets) }
+          .click { _: JQueryEventObject => scrollToElementWithBuffer(getPrevLineId, Prev) }
 
         jquery(".scroll-to-previous-line-big")
-          .click { _: JQueryEventObject => iterateToElement(getPreviousLineFromTargets, 10, Prev, currentTargets) }
+          .click { _: JQueryEventObject => iterateToElement(getPrevLineId, 10, Prev) }
 
         targetCharacterLines.each((index, line) => ContentHiding.toggleContentInJqueryElement(line))
         targetCharacterLines.each((index, line) => jquery(line).addClass("targetCharacter"))
